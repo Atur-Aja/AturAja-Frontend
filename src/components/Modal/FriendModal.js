83cdaf08fcdc335a6c0ac3550e835d28dceca88b
Faza Ghulam Ahmad <fazaghulam@gmail.com>
2021-12-04 23:45:21 +0700

    add friends modal and page loading icon when requesting

--- src/components/Modal/FriendModal.js
+++ src/components/Modal/FriendModal.js
@@ -3,23 +3,25 @@ import { useDispatch, useSelector } from "react-redux";
 import { acceptRequest, declineRequest, getAllFriendReq, getAllFriendReqSent, inviteFriend, searchUser } from "../../redux/actions/friend";
 import { IconSearch } from "../Icons";
 import { GreenButton, WhiteButton } from "../Commons/LinkButton";
+import Swal from "sweetalert2";
 
 export default function FriendModal({ onClose, show }) {
   const users = useSelector((state) => state.friend.results);
   const request = useSelector((state) => state.friend.request);
   const reqSent = useSelector((state) => state.friend.reqSent);
-  const [searchLoad, setSearchLoad] = useState(false);
-  const [invLoad, setInvLoad] = useState(false);
-  const [accLoad, setAccLoad] = useState(false);
-  const [decLoad, setDecLoad] = useState(false);
+  const [loadSearch, setloadSearch] = useState(false);
+  const [loadWaiting, setLoadWaiting] = useState(false);
+  const [loadInv, setloadInv] = useState([]);
+  const [loadAcc, setloadAcc] = useState([]);
+  const [loadDec, setloadDec] = useState([]);
 
   const [name, setName] = useState("");
 
   const dispatch = useDispatch();
   const handleSearchUser = (e) => {
     setName(e.target.value);
-    setSearchLoad(true);
-    dispatch(searchUser(e.target.value)).then(() => setSearchLoad(false));
+    setloadSearch(true);
+    dispatch(searchUser(e.target.value)).then(() => setloadSearch(false));
   };
 
   const [isInvite, setIsInvite] = useState(true);
@@ -28,22 +30,59 @@ export default function FriendModal({ onClose, show }) {
   };
   const toggleWaiting = () => {
     setIsInvite(false);
-    dispatch(getAllFriendReq());
-    dispatch(getAllFriendReqSent());
+    setLoadWaiting(true);
+    dispatch(getAllFriendReq()).then(() => setLoadWaiting(false));
+    dispatch(getAllFriendReqSent()).then(() => setLoadWaiting(false));
   };
 
-  const handleInviteUser = (user_id) => {
-    setInvLoad(true);
-    dispatch(inviteFriend(user_id)).then(() => setInvLoad(false));
+  const Toast = Swal.mixin({
+    toast: true,
+    position: "top-end",
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+      toast.addEventListener("mouseenter", Swal.stopTimer);
+      toast.addEventListener("mouseleave", Swal.resumeTimer);
+    },
+  });
+
+  const handleInviteUser = (user_id, i) => {
+    let newArr = [...loadInv];
+    newArr[i] = true;
+    setloadInv(newArr);
+    dispatch(inviteFriend(user_id)).then(() => {
+      setloadInv([]);
+      Toast.fire({
+        icon: "success",
+        title: "Successfully send invitation",
+      });
+    });
   };
-  const handleAcceptRequest = (user_id) => {
-    setAccLoad(true);
-    dispatch(acceptRequest(user_id)).then(() => setAccLoad(false));
+  const handleAcceptRequest = (user_id, i) => {
+    let newArr = [...loadAcc];
+    newArr[i] = true;
+    setloadAcc(newArr);
+    dispatch(acceptRequest(user_id)).then(() => {
+      setloadAcc([]);
+      Toast.fire({
+        icon: "success",
+        title: "Successfully accept invitation",
+      });
+    });
     dispatch(getAllFriendReq());
   };
-  const handleDeclineRequest = (user_id) => {
-    setDecLoad(true);
-    dispatch(declineRequest(user_id)).then(() => setDecLoad(false));
+  const handleDeclineRequest = (user_id, i) => {
+    let newArr = [...loadDec];
+    newArr[i] = true;
+    setloadDec(newArr);
+    dispatch(declineRequest(user_id)).then(() => {
+      setloadDec([]);
+      Toast.fire({
+        icon: "success",
+        title: "Successfully decline invitation",
+      });
+    });
     dispatch(getAllFriendReq());
   };
 
@@ -85,7 +124,7 @@ export default function FriendModal({ onClose, show }) {
                   value={name}
                 />
                 <div className="flex self-center">
-                  {searchLoad ? <div class="mr-3 loader ease-linear rounded-full border-2 border-t-2 border-gray-600 h-4 w-4" /> : null}
+                  {loadSearch ? <div class="mr-3 loader ease-linear rounded-full border-2 border-t-2 border-gray-600 h-4 w-4" /> : null}
                   <IconSearch width={"1rem"} height={"1rem"} />
                 </div>
               </div>
@@ -93,31 +132,18 @@ export default function FriendModal({ onClose, show }) {
               null}
           </div>
           <div className="px-6 py-3">
-            {isInvite &&
-              ((users?.length &&
-                users.map((list) => (
-                  <div className="flex justify-between mb-2" key={list.id}>
-                    <div className="flex">
-                      <div className="w-10 h-10 border border-black rounded-full">
-                        <img
-                          className="inline object-cover w-full h-full items-center justify-center place-self-center rounded-full"
-                          src={`http://127.0.0.1:8000/api/user/image/${list.photo}`}
-                          alt="Profile"
-                        />
-                      </div>
-                      <p className="text-lg ml-3 font-semibold self-center">{list.username}</p>
-                    </div>
-                    <div className="flex self-center">
-                      <GreenButton text={"invite"} onClick={() => handleInviteUser(list.id)} loading={invLoad} />
-                    </div>
-                  </div>
-                ))) ||
-                null)}
-            {(!isInvite && (
-              <div>
-                {(request?.length &&
-                  request.map((list) => (
-                    <div className="flex justify-between mb-3">
+            {(isInvite &&
+              ((loadSearch && (
+                <div className="h-full w-full flex flex-wrap content-center justify-center">
+                  <div className="h-2 w-2 bg-gray-500 rounded-full mr-1 animate-bounce"></div>
+                  <div className="h-2 w-2 bg-gray-500 rounded-full mr-1 animate-bounce50"></div>
+                  <div className="h-2 w-2 bg-gray-500 rounded-full mr-1 animate-bounce100"></div>
+                  <div className="h-2 w-2 bg-gray-500 rounded-full animate-bounce150"></div>
+                </div>
+              )) ||
+                (users?.length &&
+                  users.map((list, i) => (
+                    <div className="flex justify-between mb-2" key={list.id}>
                       <div className="flex">
                         <div className="w-10 h-10 border border-black rounded-full">
                           <img
@@ -129,31 +155,60 @@ export default function FriendModal({ onClose, show }) {
                         <p className="text-lg ml-3 font-semibold self-center">{list.username}</p>
                       </div>
                       <div className="flex self-center">
-                        <WhiteButton text={"ignore"} onClick={() => handleDeclineRequest(list.id)} loading={decLoad} />
-                        <GreenButton text={"accept"} onClick={() => handleAcceptRequest(list.id)} loading={accLoad} />
+                        <GreenButton text={"invite"} onClick={(e) => handleInviteUser(list.id, i)} loading={loadInv[i]} />
                       </div>
                     </div>
-                  ))) ||
-                  null}
-                {(reqSent?.length &&
-                  reqSent.map((list) => (
-                    <div className="flex justify-between mb-3">
-                      <div className="flex">
-                        <div className="w-10 h-10 border border-black rounded-full">
-                          <img
-                            className="inline object-cover w-full h-full items-center justify-center place-self-center rounded-full"
-                            src={`http://127.0.0.1:8000/api/user/image/${list.photo}`}
-                            alt="Profile"
-                          />
+                  ))))) ||
+              null}
+            {(!isInvite &&
+              ((loadWaiting && (
+                <div className="h-full w-full flex flex-wrap content-center justify-center mt-4">
+                  <div className="h-2 w-2 bg-gray-500 rounded-full mr-1 animate-bounce"></div>
+                  <div className="h-2 w-2 bg-gray-500 rounded-full mr-1 animate-bounce50"></div>
+                  <div className="h-2 w-2 bg-gray-500 rounded-full mr-1 animate-bounce100"></div>
+                  <div className="h-2 w-2 bg-gray-500 rounded-full animate-bounce150"></div>
+                </div>
+              )) || (
+                <div>
+                  {(request?.length &&
+                    request.map((list, i) => (
+                      <div className="flex justify-between mb-3">
+                        <div className="flex">
+                          <div className="w-10 h-10 border border-black rounded-full">
+                            <img
+                              className="inline object-cover w-full h-full items-center justify-center place-self-center rounded-full"
+                              src={`http://127.0.0.1:8000/api/user/image/${list.photo}`}
+                              alt="Profile"
+                            />
+                          </div>
+                          <p className="text-lg ml-3 font-semibold self-center">{list.username}</p>
                         </div>
-                        <p className="text-lg ml-3 font-semibold self-center">{list.username}</p>
+                        <div className="flex self-center">
+                          <WhiteButton text={"ignore"} onClick={() => handleDeclineRequest(list.id, i)} loading={loadDec[i]} />
+                          <GreenButton text={"accept"} onClick={() => handleAcceptRequest(list.id, i)} loading={loadAcc[i]} />
+                        </div>
                       </div>
-                      <p className="self-center text-abuTua">waiting...</p>
-                    </div>
-                  ))) ||
-                  null}
-              </div>
-            )) ||
+                    ))) ||
+                    null}
+                  {(reqSent?.length &&
+                    reqSent.map((list) => (
+                      <div className="flex justify-between mb-3">
+                        <div className="flex">
+                          <div className="w-10 h-10 border border-black rounded-full">
+                            <img
+                              className="inline object-cover w-full h-full items-center justify-center place-self-center rounded-full"
+                              src={`http://127.0.0.1:8000/api/user/image/${list.photo}`}
+                              alt="Profile"
+                            />
+                          </div>
+                          <p className="text-lg ml-3 font-semibold self-center">{list.username}</p>
+                        </div>
+                        <p className="self-center text-abuTua">waiting...</p>
+                      </div>
+                    ))) ||
+                    null}
+                </div>
+              ))) ||
               null}
           </div>
         </div>
